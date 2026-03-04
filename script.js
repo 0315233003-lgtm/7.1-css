81b4264bbb6f0d072aa1fb6e0d2939ece9e45432
0315233003-lgtm <0315233003@s.tusd1.org>
2026-03-04 17:57:51 +0000

    process

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ console.log ("Script started")
 
 // Variable to track # of clicks
 let clicks = 0;
+let clickValue = 1;
 
 //Variable to store the click display h1 element
 let clickDisplay = document.getElementById ("click-display");
+
+//Variable to store the level 
+let levelDisplay = document.getElementById("level-display");
 
 //Set the initial text for click count h1
 clickDisplay.innerText = "Clicks: " + clicks;
@@ -12,12 +16,20 @@ clickDisplay.innerText = "Clicks: " + clicks;
 //Callled each time the image is clicked
 function handleClick () {
     console.log("click");
+
     //add one to click count
-    clicks = clicks + 1;
+    clicks = clicks + clickValue;
+
     //update click count display h1
     clickDisplay.innerText = "Clicks: " + clicks;
+
+    //Check if we're at level 2
+    if (clicks == 10 ) {
+        alert("Congrats, You've reached Level 2!");
+        levelDisplay.innerText = "Level 2";
+        clickValue = 2;
+
+    }
+
 }
 
-if (clicks == 10 ) {
-    let clickValue = 2;
-}
